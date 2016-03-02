828937dff15e6648641b6728afc84242aafc1ebc
Alin <alin.ratuoiu@cs.pub.ro>
2016-03-02 08:24:57 +0200

    More images

--- js/dynamic_text_eng.js
+++ js/dynamic_text_eng.js
@@ -58,10 +58,19 @@ var spiderman_deadpoolv1 = '<ol class="rslides carousel-indicators">'
     +'</ol>' 
     
     +'<div class="carousel-inner" role="listbox">' 
-    + ' <div class="item active"> <img src="assets/spiderman.jpg" alt=""/><div class="carousel-caption"><h3>Spider-Man</h3><p>"Beyond their similar masks and color ,<b>Deadpool</b> wouldn\'t be what he\'s today without his banter during the fights."</p></div></div>'       
+    + ' <div class="item active"> <img src="assets/spiderman.jpg" alt=""/><div class="carousel-caption"><h3>Spider-Man</h3><p>Beyond their similar masks and color,<b>Deadpool</b> wouldn\'t be what he\'s today without his banter during the fights.</p></div></div>'       
     + '</div>';
 
-var wolverine_deadpoolv1 = "Both <b>Deadpool</b> and <b>Wolverine</b> got their trademark abilities and mental derangements from the Weapon X program. And from all the verious supergroups in Marvel Comics he's most closely associated with the X-Men over the years. Not surprising, having debuted in one of their books.";
+var wolverine_deadpoolv1 =
+    '<ol class="rslides carousel-indicators">'  
+        +'<li data-target="#detail_text" data-slide-to="0" class="active"></li>'
+        +'<li data-target="#detail_text" data-slide-to="1"></li>'  
+    +'</ol>' 
+    
+    +'<div class="carousel-inner" role="listbox">' 
+    + ' <div class="item active"> <img src="assets/wolverine1.jpg" alt=""/><div class="carousel-caption"><h3>Wolverine</h3><p>Both <b>Deadpool</b> and <b>Wolverine</b> got their trademark abilities and mental derangements from the Weapon X program.</p></div></div>'
+    + ' <div class="item"> <img src="assets/wolverine2.jpg" alt=""/><div class="carousel-caption"><p>And from all the verious supergroups in Marvel Comics he\'s most closely associated with the X-Men over the years. Not surprising, having debuted in one of their books.</p></div></div>'       
+    + '</div>' 
 
 var deadpoolv1 = "<b>Deadpool</b> was created by <b>Rob Liefeld</b>, making his first appearance in #98 of the New Mutants from 1991, written by <b>Fabian Nicieza</b>. He appeared as a synthesis of multiple chracters which interested <b>Liefeld</b> at the time resulting in a talkative mercenary, armed with swords, guns, an extraordinay healing factor coupled with an ugly mug and a defective teleportation belt. This is the version kept for different appearences and two miniseries, none of which remarked itself through humor or metatextuality, other than the main characters mean spirited pop-culture references from his dialogue, which were more of an warped verion of Spidey's trademark banter."
 
